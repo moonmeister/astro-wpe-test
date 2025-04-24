37bc6c5bdc83bc8527ffec520a8302706d2808ec
Alex Moon <moonmeister@users.noreply.github.com>
2025-04-24 12:03:15 -0700

    middleware

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+import { defineMiddleware, lo } from "astro:middleware";
+import { randomUUID } from "node:crypto";
+
+/**
+ * @type {import("astro").MiddlewareHandler}
+ */
+export const onRequest = defineMiddleware((context, next) => {
+  // This middleware runs on every request
+  const requestId = randomUUID();
+
+  context.locals.uuid = requestId;
+
+  // Call the next middleware or route handler
+  return next();
+});
