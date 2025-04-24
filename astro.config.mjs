9b30d84cb5cf15f2df6b386f6673b1b4b32a1cef
Alex Moon <moonmeister@users.noreply.github.com>
2025-04-24 16:50:09 -0700

    Add sessions

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,24 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  session: {
+    /**
+     * FS driver is the default driver and stores session data in the file system. This does not work well in serverless environments like the WP Engine Headless Platform.
+     * Here is an example of how to use the Redis driver.
+     * see: https://unstorage.unjs.io/drivers/redis for more information.
+     * driver: "redis",
+     * options: {
+     *   url: "redis://default:password@redis.server.com:6379",
+     * },
+     *
+     */
+    driver: "fs",
+    cookie: {
+      name: "wpe-astro-test-session",
+      sameSite: "strict",
+      secure: true,
+    },
+  },
 
   integrations: [svelte()],
 });
